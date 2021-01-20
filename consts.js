15b7845220b8124f993b0809c5bdee6fcbb06fa1
saharhash <saharhashai@gmail.com>
2021-01-20 15:43:34 +0200

    Adjusted pricing for instruct engines (now costs as their non-instruct equivalents)

--- consts.js
+++ consts.js
@@ -1,4 +1,4 @@
-const enginesCreditsMapping = {'davinci': 0.06, 'curie': 0.006, 'babbage': 0.0012, 'ada': 0.0008};
-const freeEngines = ['content-filter-alpha-c4', 'instruct-curie-beta', 'instruct-davinci-beta'];
+const enginesCreditsMapping = {'davinci': 0.06, 'davinci-instruct-beta': 0.06, 'curie': 0.006, 'curie-instruct-beta': 0.006, 'babbage': 0.0012, 'ada': 0.0008};
+const freeEngines = ['content-filter-alpha-c4'];
 const maxTokenSize = 2048;
 const pageLoadWaitIntervals = 2000;
